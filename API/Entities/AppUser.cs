44849c8e13fe9153adac952db4d929f08d84de48
Ianka Karúzia <iankakaruzia@gmail.com>
2022-02-22 00:07:08 -0300

    add likes functionality

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,3 @@
-using API.Extensions;
-
 namespace API.Entities;
 
 public class AppUser
@@ -19,9 +17,6 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public ICollection<Photo> Photos { get; set; }
-
-    // public int GetAge()
-    // {
-    //     return DateOfBirth.CalculateAge();
-    // }
+    public ICollection<UserLike> LikedByUsers { get; set; }
+    public ICollection<UserLike> LikedUsers { get; set; }
 }
